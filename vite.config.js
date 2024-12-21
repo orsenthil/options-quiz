6823de30e8153bb442da0bcd2ff8aec68756ce1f
Senthil Kumaran <senthil@python.org>
2024-12-21 06:37:03 -0800

    Add Options for Custom Sub-Domain.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/options-quiz/', // Replace with your repository name
+  // base: '/options-quiz/', // Replace with your repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
